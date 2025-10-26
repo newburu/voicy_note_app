35d0eb440f02776fcfe66ee18603af278d05bfef
Newburu <newburu@gmail.com>
2025-10-26 13:01:18 +0900

    Add authentication logic to layout and ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,18 @@ class ApplicationController < ActionController::Base
 
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
+
+  # session[:user_id] があれば、現在のユーザー（@current_user）を取得
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  # ビュー（HTML）側でも current_user メソッドを使えるようにする
+  helper_method :current_user
+
+  # ログインしているか確認するメソッド
+  def logged_in?
+    current_user.present?
+  end
+  helper_method :logged_in?
 end
